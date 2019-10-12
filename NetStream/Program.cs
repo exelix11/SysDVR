4c0dbf2723ef8a569fdfd29295f255198f684dc9
exelix <13405476+exelix11@users.noreply.github.com>
2019-10-12 15:54:04 +0200

    Better args handling, use any program as stdin argument

--- NetStream/Program.cs
+++ NetStream/Program.cs
@@ -1,4 +1,4 @@
-﻿#define PRINT_DEBUG
+﻿//#define PRINT_DEBUG
 #define PLAY_STATS
 
 using System;
@@ -83,17 +83,18 @@ namespace NetStream
 			}
 		}
 
-		class StdInMPV : IOutTarget
+		class StdInTarget : IOutTarget
 		{
 			Process proc;
 
-			public StdInMPV(string path, string args)
+			public StdInTarget(string path, string args)
 			{
 				ProcessStartInfo p = new ProcessStartInfo()
 				{
 					Arguments = " - " + args,
 					FileName = path,
 					RedirectStandardInput = true,
+					RedirectStandardOutput = true,
 				};
 				proc = Process.Start(p);
 			}
@@ -136,17 +137,22 @@ namespace NetStream
 
 		static bool StreamLoop(IOutTarget Target, UsbDevStream stream, StreamKind kind)
 		{
+			bool PreventDesync = false;
 #if PLAY_STATS
 			Stopwatch ThreadTimer = new Stopwatch();
 			long TransfersPerSecond = 0;
 			long BytesPerSecond = 0;
 			ThreadTimer.Start();
 
-			void UpdatePlayStats() 
+			void UpdatePlayStats()
 			{
 				if (ThreadTimer.ElapsedMilliseconds < 1000) return;
 				ThreadTimer.Stop();
 				Console.WriteLine($"{kind} stream: {TransfersPerSecond} - {BytesPerSecond / ThreadTimer.ElapsedMilliseconds} KB/s");
+
+				if (BytesPerSecond / ThreadTimer.ElapsedMilliseconds < 10)
+					PreventDesync = true;
+
 				TransfersPerSecond = 0;
 				BytesPerSecond = 0;
 				ThreadTimer.Restart();
@@ -168,7 +174,7 @@ namespace NetStream
 			ArrayPool<byte> sh = ArrayPool<byte>.Create();
 			byte[] data = null;
 
-			int ReadToSharedArray() 
+			int ReadToSharedArray()
 			{
 				SizeBuf[0] = SizeBuf[1] = SizeBuf[2] = SizeBuf[3] = 0;
 				stream.MillisTimeout = 500;
@@ -184,41 +190,48 @@ namespace NetStream
 				return actualsize;
 			}
 
-			void FreeBuffer() 
+			void FreeBuffer()
 			{
 				if (data != null) sh.Return(data);
 				data = null;
 			}
 
-			#if !DEBUG
+#if !DEBUG
 			try
 			{
-			#endif
-				while (!Console.KeyAvailable)
+#endif
+			while (!Console.KeyAvailable)
+			{
+				if (PreventDesync)
 				{
-					stream.Write(ReqMagic);
-
-					var size = ReadToSharedArray();
-					if (size > MaxBufSize || size <= 0)
-					{
-						Console.WriteLine($"Discarding {kind} packet of size {size}");
-						stream.Flush();
-					}
-					else
-					{
-						Target.SendData(data, 0, (int)size);
-						FreeBuffer();
-						#if PRINT_DEBUG
-							Console.WriteLine($"video {size}");
-						#endif
-						
-						#if PLAY_STATS
-							TransfersPerSecond++;
-							BytesPerSecond += size;
-							UpdatePlayStats();
-						#endif
-					}
+					Console.WriteLine("Preventing desync");
+					System.Threading.Thread.Sleep(1000);
+					PreventDesync = false;
 				}
+
+				stream.Write(ReqMagic);
+
+				var size = ReadToSharedArray();
+				if (size > MaxBufSize || size <= 0)
+				{
+					Console.WriteLine($"Discarding {kind} packet of size {size}");
+					stream.Flush();
+				}
+				else
+				{
+					Target.SendData(data, 0, (int)size);
+					FreeBuffer();
+#if PRINT_DEBUG
+					Console.WriteLine($"video {size}");
+#endif
+
+#if PLAY_STATS
+					TransfersPerSecond++;
+					BytesPerSecond += size;
+					UpdatePlayStats();
+#endif
+				}
+			}
 #if !DEBUG
 				
 			}
@@ -231,65 +244,90 @@ namespace NetStream
 			return false;
 		}
 
+		static void PrintGuide()
+		{
+			Console.WriteLine("Usage: \r\n" +
+					"NetStream video <stream config for video> audio <stream config for audio>\r\n" +
+					"You can omit the stream you don't want\r\n" +
+					"Stream config is one of the following:\r\n" +
+					" - tcp <port> : stream the data over a the network on the specified port.\r\n" +
+					" - file <file name> : stores the received data to a file\r\n" +
+					"   The format is raw h264 data for video and uncompressed s16le stereo 48kHz samples for sound\r\n" +
+					" - stdin <executable path> args <program arguments> : Pipes the received data to another program, <executable path> is the other program's path and <program arguments> are the args to pass to the target program, you can omit args if the program doesn't need any configuration\r\n" +
+					" - mpv <mpv player path> : same as stdin but automatically configures args for mpv\r\n" +
+					"It is not recommended to stream audio and video at the same time\r\n" +
+					"Note that tcp mode will wait until a program connects\r\n\r\n" +
+					"Example commands: \r\n" +
+					"NetStream audio mpv C:/programs/mpv/mpv : Plays audio via mpv located at C:/programs/mpv/mpv, video is ignored\r\n" +
+					"NetStream video mpv ./mpv audio mpv ./mpv : Plays video and audio via mpv (path has to be specified twice)\r\n" +
+					"NetStream video mpv ./mpv args \"--cache=no --cache-secs=0\" : Plays video in mpv disabling cache, audio is ignored\r\n" +
+					"NetStream video tcp 1337 audio file C:/audio.raw : Streams video over port 1337 while saving audio to disk\r\n\r\n" +
+					"Opening raw files in mpv: \r\n" +
+					"mpv videofile.264 --no-correct-pts --fps=30 --cache=no --cache-secs=0\r\n" +
+					"mpv audiofile.raw --no-video --demuxer=rawaudio --demuxer-rawaudio-rate=48000\r\n" +
+					"(you can also use tcp://localhost:<port> instead of the file name to open the tcp stream)\r\n\r\n" +
+					"Info to keep in mind:\r\n" +
+					"Streaming works only with games that have game recording enabled.\r\n" +
+					"If the video is very delayed or lagging try going to the home menu for a few seconds to force it to re-synchronize.\r\n" +
+					"After disconnecting and reconnecting the usb wire the stream may not start right back, go to the home menu for a few seconds to let the sysmodule drop the last usb packets.");
+			Console.ReadLine();
+		}
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("SysVStream - 1.0 by exelix");
 			Console.WriteLine("https://github.com/exelix11/SysVStream \r\n");
 			if (args.Length < 3)
 			{
-				Console.WriteLine("Usage: \r\n" +
-				"NetStream <video config> <audio conifg>\r\n" +
-				"Supported configurations are: \r\n" +
-				" - tcp <port> : stream the data over a the network on the specified port.\r\n" +
-				" - file <file name> : stores the received data to a file\r\n" +
-				"   The format is raw h264 data for video and uncompressed s16le stereo 48kHz samples for sound\r\n" +
-				" - mpv <mpv player path> : streams the received data to mpv player via stdin\r\n" +
-				" - null : ignore this stream\r\n" +
-				"It is not recommended to stream audio and video at the same time\r\n" +
-				"Note that tcp initialization will block untill a program connects\r\n\r\n" +
-				"Example commands: \r\n" +
-				"NetStream null mpv C:/programs/mpv/mpv : Plays audio via mpv located at C:/programs/mpv/mpv, video is ignored\r\n" +
-				"NetStream mpv C:/programs/mpv/mpv mpv C:/programs/mpv/mpv : Plays video and audio via mpv (path has to be specified twice)\r\n" +
-				"NetStream tcp 1337 file C:/audio.raw : Streams video over port 1337 while saving audio to disk\r\n\r\n" +
-				"Opening raw files in mpv: \r\n" +
-				"mpv videofile.264 --no-correct-pts --fps=30 --cache=no --cache-secs=0\r\n" +
-				"mpv audiofile.raw --no-video --demuxer=rawaudio --demuxer-rawaudio-rate=48000\r\n" +
-				"(you can also use tcp://localhost:<port> instead of the file name to open the tcp stream)\r\n\r\n" +
-				"Info to keep in mind:\r\n" +
-				"Streaming works only with games that have game recording enabled.\r\n" +
-				"If the video is very delayed or lagging try going to the home menu for a few seconds to force it to re-synchronize.\r\n" +
-				"After disconnecting and reconnecting the usb wire the stream may not start right back, go to the home menu for a few seconds to let the sysmodule drop the last usb packets.");
-				Console.ReadLine();
+				PrintGuide();
 				return;
 			}
 
 			IOutTarget VTarget = null;
 			IOutTarget ATarget = null;
 
-			int AudioArgsStart = 2;
-
-			if (args[0] == "file")
-				VTarget = new OutFileTarget(args[1]);
-			else if (args[0] == "tcp")
-				VTarget = new TCPTarget(System.Net.IPAddress.Any, int.Parse(args[1]));
-			else if (args[0] == "mpv")
-				VTarget = new StdInMPV(args[1], "--no-correct-pts --fps=30");
-			else if (args[0] == "null")
+			void ParseTargetArgs(int baseIndex, ref IOutTarget t)
 			{
-				VTarget = null;
-				AudioArgsStart = 1;
-			}
-			else throw new Exception("Unknown video method");
+				switch (args[baseIndex + 1])
+				{
+					case "mpv":
+					case "stdin":
+						{
+							string fargs = "";
+							if (args[baseIndex + 1] == "mpv") 
+								fargs = args[baseIndex] == "video" ? "- --no-correct-pts --fps=30 " : "- --no-video --demuxer=rawaudio --demuxer-rawaudio-rate=48000 ";
 
-			if (args[AudioArgsStart] == "file")
-				ATarget = new OutFileTarget(args[AudioArgsStart + 1]);
-			else if (args[AudioArgsStart] == "tcp")
-				ATarget = new TCPTarget(System.Net.IPAddress.Any, int.Parse(args[AudioArgsStart + 1]));
-			else if (args[AudioArgsStart] == "null")
-				ATarget = null;
-			else if (args[AudioArgsStart] == "mpv")
-				ATarget = new StdInMPV(args[AudioArgsStart + 1], "--no-video --demuxer=rawaudio --demuxer-rawaudio-rate=48000");
-			else throw new Exception("Unknown audio method");
+							if (args.Length > baseIndex + 4 && args[baseIndex + 3] == "args")
+								fargs += args[baseIndex + 4];
+
+							t = new StdInTarget(args[baseIndex + 2], fargs);
+
+							break;
+						}
+					case "tcp":
+						{
+							int port = int.Parse(args[baseIndex + 2]);
+							Console.WriteLine($"Waiting for a client to connect on {port} ...");
+							t = new TCPTarget(System.Net.IPAddress.Any, port);
+							break;
+						}
+					case "file":
+						t = new OutFileTarget(args[baseIndex + 2]);
+						break;
+					default:
+						throw new Exception($"{args[baseIndex + 1]} is not a valid video mode");
+				}
+			}
+
+			{
+				int index = Array.IndexOf(args, "video");
+				if (index >= 0) ParseTargetArgs(index, ref VTarget);
+				index = Array.IndexOf(args, "audio");
+				if (index >= 0) ParseTargetArgs(index, ref ATarget);
+			}
+
+			if (VTarget == null && ATarget == null)
+				throw new Exception("Specify at least a video or audio target");
 
 			var stream = GetDevice();
 
