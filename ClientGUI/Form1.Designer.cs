a555febcc2b505ebe09842b8a140dff3148d1b17
exelix <ex.elix@libero.it>
2020-05-22 12:57:52 +0200

    Add icons, finally

--- ClientGUI/Form1.Designer.cs
+++ ClientGUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.button3 = new System.Windows.Forms.Button();
@@ -361,6 +362,7 @@
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MaximumSize = new System.Drawing.Size(646, 493);
 			this.MinimizeBox = false;
