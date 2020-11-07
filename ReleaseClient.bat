f3c80e500c3123dfc5548c924f213fa2c153f75f
exelix <13405476+exelix11@users.noreply.github.com>
2020-11-07 12:55:01 +0100

    Update client build script to build dependencies. Remove binaries

--- ReleaseClient.bat
+++ ReleaseClient.bat
@@ -2,10 +2,22 @@
 
 set PATH=C:\Program Files\7zip;C:\Program Files\7-Zip\;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\;%PATH%
 
-cd Client
+cd Libs\SharpRTSP\RTSP
+dotnet pack || goto error
+move bin\Debug\SharpRTSP.0.0.1.nupkg ..\..\Built\
+
+cd ..\..\LibUsbDotNet\src\LibUsbDotNet\
+dotnet pack || goto error
+move bin\Debug\LibUsbDotNet.*.nupkg ..\..\..\Built\
+
+cd ..\..\..\..\Client.Native
+dotnet pack || goto error
+REM Client.Native is automatically copied to Libs\Built
+
+cd ..\Client
 dotnet publish -c Release || goto error
 
-cd ../ClientGUI
+cd ..\ClientGUI
 call VsDevCmd.bat
 msbuild ClientGUI.csproj -p:Configuration=Release || goto error
 
@@ -13,8 +25,7 @@ cd ..
 
 move Client\bin\Release\netcoreapp3.1\SysDVR-ClientGUI.exe Client\bin\Release\netcoreapp3.1\publish\SysDVR-ClientGUI.exe 
 
-del Client\bin\Release\netcoreapp3.1\publish\NLog.xml
-del Client\bin\Release\netcoreapp3.1\publish\Rtsp.pdb
+del Client\bin\Release\netcoreapp3.1\publish\*.pdb
 
 7z a Client.7z .\Client\bin\Release\netcoreapp3.1\publish\*
 
