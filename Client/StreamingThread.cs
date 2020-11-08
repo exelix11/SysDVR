899229c5d441d5eb91d3e7c605d7cddf4e38c822
exelix <ex.elix@libero.it>
2020-11-09 00:10:46 +0100

    Fix scaling on OSX. Fix player window getting stuck when no new frame is available.

--- Client/StreamingThread.cs
+++ Client/StreamingThread.cs
@@ -45,6 +45,40 @@ namespace SysDVR.Client
 		bool ReadPayload(byte[] buffer, int length);
 	}
 
+#if DEBUG
+	class StubSource : IStreamingSource
+	{
+		public bool Logging { get; set; }
+
+		public void Flush()
+		{
+			
+		}
+
+		public bool ReadHeader(byte[] buffer) =>
+			throw new NotImplementedException();
+
+		public bool ReadPayload(byte[] buffer, int length) =>
+			throw new NotImplementedException();
+
+		public void StopStreaming()
+		{
+			
+		}
+
+		public void UseCancellationToken(CancellationToken tok)
+		{
+
+		}
+
+		public void WaitForConnection()
+		{
+			while (true)
+				Thread.Sleep(1000);
+		}
+	}
+#endif
+
 	class StreamingThread : IDisposable
 	{
 		public static bool Logging;
