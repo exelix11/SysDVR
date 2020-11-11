ad23faf2ee3f07782433c33560b58956138166fc
exelix <ex.elix@libero.it>
2020-11-11 13:49:50 +0100

    Add toolbox.json

--- ReleaseSysmodule.sh
+++ ReleaseSysmodule.sh
@@ -18,6 +18,7 @@ mkdir -p ../SysmoduleRelease/Main/config/sysdvr
 echo . > ../SysmoduleRelease/Main/config/sysdvr/rtsp
 cp sysmodule.nsp ../SysmoduleRelease/Main/atmosphere/contents/00FF0000A53BB665/exefs.nsp
 cp sysmodule.elf ../SysmoduleRelease/Debug/main.elf
+cp toolbox.json ../SysmoduleRelease/Main/atmosphere/contents/00FF0000A53BB665/
 echo . > ../SysmoduleRelease/Main/atmosphere/contents/00FF0000A53BB665/flags/boot2.flag
 cp ../SysDVRConfig/SysDVR-conf.nro ../SysmoduleRelease/Main/switch/SysDVR-conf.nro
 7z a ../SysmoduleRelease/SysDVR.zip ../SysmoduleRelease/Main/*
@@ -27,6 +28,7 @@ make -j4 DEFINES="-DRELEASE -DUSB_ONLY"
 mkdir -p ../SysmoduleRelease/USB/atmosphere/contents/00FF0000A53BB665/flags 
 cp sysmodule.nsp ../SysmoduleRelease/USB/atmosphere/contents/00FF0000A53BB665/exefs.nsp
 cp sysmodule.elf ../SysmoduleRelease/Debug/usbOnly.elf
+cp toolbox.usb.json ../SysmoduleRelease/USB/atmosphere/contents/00FF0000A53BB665/toolbox.json
 echo . > ../SysmoduleRelease/USB/atmosphere/contents/00FF0000A53BB665/flags/boot2.flag
 7z a ../SysmoduleRelease/USB.zip ../SysmoduleRelease/USB/*
 
