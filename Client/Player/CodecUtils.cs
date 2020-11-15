1e878c053ff10e7b1eec98fe225a25b2111fa130
exelix <ex.elix@libero.it>
2020-11-15 12:25:05 +0100

    Initial hardware accelerated decoer implementation, add error messages when assert fails.

---
+++ Client/Player/CodecUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+using static FFmpeg.AutoGen.ffmpeg;
+
+namespace SysDVR.Client.Player
+{
+	public static class CodecUtils
+	{
+		public static unsafe void PrintAllCodecs() 
+		{
+			void* i = null;
+			AVCodec* c = null;
+
+			Console.WriteLine("Available codecs: ");
+			while ((c = av_codec_iterate(&i)) != null)
+			{
+				if (av_codec_is_decoder(c) != 0)
+				{
+					var desc = Marshal.PtrToStringAnsi((IntPtr)c->long_name);
+					var name = Marshal.PtrToStringAnsi((IntPtr)c->name);
+
+					if (!desc.Contains("264") && !name.Contains("264"))
+						continue;
+
+					Console.WriteLine($"Name: {name} Description: {desc}");
+				}
+			}
+		}
+		
+	}
+}
