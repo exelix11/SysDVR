280ffc249e7a6abf3c88c108c2f84b6ce42e4ea9
exelix <13405476+exelix11@users.noreply.github.com>
2020-11-19 23:17:06 +0100

    Add warning for 32-bit versions of dotnet on windows

--- Client/Player/Player.cs
+++ Client/Player/Player.cs
@@ -61,6 +61,16 @@ namespace SysDVR.Client.Player
 			HasVideo ? new H264StreamTarget() : null,
 			HasAudio ? new AudioStreamTarget() : null)
 		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !Environment.Is64BitProcess)
+			{
+				Console.WriteLine(
+					"WARNING: You seem to be running the 32-bit version of .NET, on Windows this is NOT supported due to ffmpeg not providing official 32-bit versions of their libs.\r\n" +
+					"If you're running a 64-bit install of Windows (check your pc info) uninstall .NET and install the x64 version from Microsoft's website.\r\n" +
+					"If you're running a 32-bit install of Windows you should upgrade your PC. Alternatively you need to find a 32-bit build of ffmpeg libs and copy them in the SysDVR-Client folder after that you can ignore this warning.\r\n" +
+					"Continuing without the 32-bit libs WILL crash with a missing libraries/wrong library format error."
+				);
+			}
+
 			player = new Player(this, hwAcc, codecName, quality);
 		}
 
