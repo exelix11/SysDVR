2801690c7aeddf8eda971c26e50c68a659d6d6eb
exelix <ex.elix@libero.it>
2020-11-19 11:28:40 +0100

    Add "best" scaling option to the GUI

--- ClientGUI/ModesUI/PlayStreamControl.Designer.cs
+++ ClientGUI/ModesUI/PlayStreamControl.Designer.cs
@@ -29,25 +29,37 @@
 		private void InitializeComponent()
 		{
 			this.cbHwAcc = new System.Windows.Forms.CheckBox();
+			this.cbBestScaling = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
 			// 
 			// cbHwAcc
 			// 
-			this.cbHwAcc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.cbHwAcc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.cbHwAcc.Location = new System.Drawing.Point(5, 5);
 			this.cbHwAcc.Name = "cbHwAcc";
-			this.cbHwAcc.Size = new System.Drawing.Size(429, 43);
+			this.cbHwAcc.Size = new System.Drawing.Size(429, 41);
 			this.cbHwAcc.TabIndex = 5;
 			this.cbHwAcc.Text = "Use hardware acceleration for decoding (not recommended as auto detection may fai" +
     "l, it\'s better to use the --decoder option from cmd)";
 			this.cbHwAcc.UseVisualStyleBackColor = true;
 			// 
+			// cbBestScaling
+			// 
+			this.cbBestScaling.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbBestScaling.Location = new System.Drawing.Point(5, 44);
+			this.cbBestScaling.Name = "cbBestScaling";
+			this.cbBestScaling.Size = new System.Drawing.Size(429, 35);
+			this.cbBestScaling.TabIndex = 6;
+			this.cbBestScaling.Text = "Use anisotropic filtering for scaling (direct3d renderer only)";
+			this.cbBestScaling.UseVisualStyleBackColor = true;
+			// 
 			// PlayStreamControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.cbBestScaling);
 			this.Controls.Add(this.cbHwAcc);
 			this.Name = "PlayStreamControl";
 			this.Size = new System.Drawing.Size(437, 90);
@@ -57,5 +69,6 @@
 
 		#endregion
 		private System.Windows.Forms.CheckBox cbHwAcc;
+		private System.Windows.Forms.CheckBox cbBestScaling;
 	}
 }
