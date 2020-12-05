53361fd8fff4f25153be2c6748ca89faaedbc835
exelix11 <ex.elix@libero.it>
2020-12-05 23:19:20 +0100

    Update usbComms implementation to match libnx master

--- sysmodule/source/USB/UsbComms.h
+++ sysmodule/source/USB/UsbComms.h
@@ -20,14 +20,15 @@ typedef struct {
 Result UsbCommsInitialize(struct usb_device_descriptor* device_descriptor, u32 num_interfaces, const UsbInterfaceDesc* infos);
 void UsbCommsExit(void);
 
-size_t UsbCommsTransfer(u32 interface, u32 endpoint, UsbDirection dir, const void* buffer, size_t size, u64 timeout);
+size_t UsbCommsReadEx(void* buffer, size_t size, u32 interface, u64 timeout);
+size_t UsbCommsWriteEx(const void* buffer, size_t size, u32 interface, u64 timeout);
 
-static inline size_t UsbSerialRead(const UsbPipe* stream, const void* buf, u32 bufSize, u64 timeout)
+static inline size_t UsbSerialRead(const UsbPipe* stream, void* buf, u32 bufSize, u64 timeout)
 {
-	return UsbCommsTransfer(stream->interface, stream->ReadEP, UsbDirection_Read, buf, bufSize, timeout);
+	return UsbCommsReadEx(buf, bufSize, stream->interface, timeout);
 }
 
 static inline size_t UsbSerialWrite(const UsbPipe* stream, const void* buf, u32 bufSize, u64 timeout)
 {
-	return UsbCommsTransfer(stream->interface, stream->WriteEP, UsbDirection_Write, buf, bufSize, timeout);
+	return UsbCommsWriteEx(buf, bufSize, stream->interface, timeout);
 }
