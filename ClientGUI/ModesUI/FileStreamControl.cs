56fe4647b3dcebf487e83743689042d9003429ae
exelix11 <ex.elix@libero.it>
2021-02-21 22:51:23 +0100

    Add save to mp4 option in the GUI

--- ClientGUI/ModesUI/FileStreamControl.cs
+++ ClientGUI/ModesUI/FileStreamControl.cs
@@ -22,7 +22,7 @@ namespace SysDVRClientGUI
 
 		public string GetCommandLine()
 		{
-			if (string.IsNullOrWhiteSpace(tbVideoFile.Text) || !Directory.Exists(tbVideoFile.Text))
+			if (string.IsNullOrWhiteSpace(tbVideoFile.Text))
 				throw new Exception("Select a valid path to save the video data first");			
 
 			return $"--file \"{tbVideoFile.Text}\"";
@@ -35,14 +35,10 @@ namespace SysDVRClientGUI
 
 		private void btnVideo_Click(object sender, EventArgs e)
 		{
-			FolderBrowserDialog dir = new FolderBrowserDialog();
-			if (dir.ShowDialog() == DialogResult.OK)
+			SaveFileDialog sav = new SaveFileDialog() { Filter = "mp4 file|*.mp4" };
+			if (sav.ShowDialog() == DialogResult.OK)
 			{
-				if (File.Exists(Path.Combine(dir.SelectedPath, "video.h264")) || File.Exists(Path.Combine(dir.SelectedPath, "audio.raw")))
-					if (MessageBox.Show("The folder you selected already contains a video.h264 or audio.raw file, these will be overwritten, do you want to continue ?", "Warning", MessageBoxButtons.YesNo) == DialogResult.No)
-						return;
-
-				tbVideoFile.Text = dir.SelectedPath;
+				tbVideoFile.Text = sav.FileName;
 			}
 		}
 	}
