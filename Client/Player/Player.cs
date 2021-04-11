35285b480cdc90f997f2f933adc65629cc74535c
exelix <13405476+exelix11@users.noreply.github.com>
2021-04-11 13:29:05 +0200

    Fix typo

--- Client/Player/Player.cs
+++ Client/Player/Player.cs
@@ -604,31 +604,31 @@ namespace SysDVR.Client.Player
 		public static void AssertEqual(this int code, int expectedValue, Func<string> MessageFun = null)
 		{
 			if (code != expectedValue)
-				throw new Exception($"Asserition failed {code} != {expectedValue} : {(MessageFun?.Invoke() ?? "Unknown error:")}");
+				throw new Exception($"Assertion failed {code} != {expectedValue} : {(MessageFun?.Invoke() ?? "Unknown error:")}");
 		}
 
 		public static void Assert(this int code, Func<string> MessageFun = null)
 		{
 			if (code != 0)
-				throw new Exception($"Asserition failed: {code} {(MessageFun?.Invoke() ?? "Unknown error")}");
+				throw new Exception($"Assertion failed: {code} {(MessageFun?.Invoke() ?? "Unknown error")}");
 		}
 
 		public static void AssertNotNeg(this int code, Func<string> MessageFun = null)
 		{
 			if (code < 0)
-				throw new Exception($"Asserition failed: {code} {(MessageFun?.Invoke() ?? "Unknown error")}");
+				throw new Exception($"Assertion failed: {code} {(MessageFun?.Invoke() ?? "Unknown error")}");
 		}
 
 		public static void Assert(this int code, string Message)
 		{
 			if (code != 0)
-				throw new Exception($"Asserition failed: {code} {Message}");
+				throw new Exception($"Assertion failed: {code} {Message}");
 		}
 
 		public static IntPtr Assert(this IntPtr val, Func<string> MessageFun = null)
 		{
 			if (val == IntPtr.Zero)
-				throw new Exception($"Asserition failed: pointer is null {(MessageFun?.Invoke() ?? "Unknown error")}");
+				throw new Exception($"Assertion failed: pointer is null {(MessageFun?.Invoke() ?? "Unknown error")}");
 			return val;
 		}
 	}
