05aa52249e2e47aa7ce21b073fc5fd12cf2e6b61
exelix11 <13405476+exelix11@users.noreply.github.com>
2022-04-23 11:16:34 +0200

    New USB protocol, upgrade to .NET6 for osx on arm

--- Client/Sources/UsbStreaming.cs
+++ Client/Sources/UsbStreaming.cs
@@ -2,8 +2,11 @@
 using LibUsbDotNet.Main;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 namespace SysDVR.Client.Sources
@@ -154,21 +157,51 @@ namespace SysDVR.Client.Sources
 	abstract class UsbStreamingSourceBase : IStreamingSource
 	{
 		protected static readonly byte[] USBMagic = { 0xAA, 0xAA, 0xAA, 0xAA };
-		public bool Logging { get; set; }
 
-		protected UsbEndpointReader reader;
+		public bool Logging { get; set; }
+		CancellationToken Token; 
+
+		// TODO: Remove tracing code
+		readonly private TimeTrace tracer = new();
+		protected readonly string streamKind;
+		protected TimeTrace BeginTrace(string extra = "", [CallerMemberName] string funcName = null) => 
+			tracer.Begin(streamKind, extra, funcName);
+
+		protected class TimeTrace : IDisposable
+        {
+			Stopwatch sw = new Stopwatch();
+			StringBuilder sb = new(); 
+
+			public TimeTrace Begin(string kind, string extra, string funcname) 
+			{
+				sb.Clear();
+				sb.Append($"[{kind}] [{funcname}] {extra} ");
+				sw.Restart();
+				return this;
+			}
+
+			public void Mark(string name) {
+				sb.Append($"{sw.ElapsedMilliseconds} ms | {name} ");
+				sw.Restart();
+			}
+
+			public void Dispose()
+            {
+				sw.Stop();
+				sb.Append($"{sw.ElapsedMilliseconds} ms");
+				Console.WriteLine(sb.ToString());
+            }
+        }
+
+        protected UsbEndpointReader reader;
 		protected UsbEndpointWriter writer;
 		protected UsbContext context;
 
 		public UsbStreamingSourceBase(StreamKind kind, UsbContext context)
 		{
 			this.context = context;
+			this.streamKind = kind.ToString();
 			(reader, writer) = context.GetForInterface(kind);
-		}
-
-		public void WaitForConnection()
-		{
-
 		}
 
 		public void StopStreaming()
@@ -176,40 +209,54 @@ namespace SysDVR.Client.Sources
 			context.MarkInterfaceClosed();
 		}
 
-		public void UseCancellationToken(CancellationToken tok)
+		public void WaitForConnection() 
 		{
+			while (!Token.IsCancellationRequested) 
+			{
+				using var trace = BeginTrace();
 
+				var err = writer.Write(USBMagic, 1000, out int _);
+				if (err != LibUsbDotNet.Error.Success)
+				{
+					Thread.Sleep(1000);
+					Console.WriteLine($"Warning: console handshake failed ({err}). Try unplugging your console and restart SysDVR o");
+					continue;
+				}
+
+				return;
+			}
 		}
 
-		public abstract void Flush();
+		public virtual void Flush() 
+		{
+			// Wait some time so the switch side timeouts
+			Thread.Sleep(2000);
+			// Then attempt to connect again
+			WaitForConnection();
+		}
+
+		// Provided by the underlying implementation
 		public abstract bool ReadHeader(byte[] buffer);
 		public abstract bool ReadPayload(byte[] buffer, int length);
+
+		public void UseCancellationToken(CancellationToken tok)
+		{
+			Token = tok;
+		}
 	}
 
 	class UsbStreamingSourceWinUsb : UsbStreamingSourceBase
 	{
 		public UsbStreamingSourceWinUsb(StreamKind kind, UsbContext context) : base(kind, context) { }
 
-		public override void Flush()
-		{
-			reader.ReadFlush();
-		}
-
 		public override bool ReadHeader(byte[] buffer)
 		{
-			var err = writer.Write(USBMagic, 100, out int _);
-			if (err != LibUsbDotNet.Error.Success)
-			{
-				if (Logging)
-					Console.WriteLine($"Warning: winusb error {err} while requesting data");
-				return false;
-			}
+			using var trace = BeginTrace();
 
-			err = reader.Read(buffer, 0, PacketHeader.StructLength, 100, out int _);
+			var err = reader.Read(buffer, 0, PacketHeader.StructLength, 200, out int _);
 			if (err != LibUsbDotNet.Error.Success)
 			{
-				if (Logging)
-					Console.WriteLine($"Warning: winusb error {err} while reading header");
+				Console.WriteLine($"Warning: winusb error {err} while reading header");
 				return false;
 			}
 
@@ -218,11 +265,10 @@ namespace SysDVR.Client.Sources
 
 		public override bool ReadPayload(byte[] buffer, int length)
 		{
-			var err = reader.Read(buffer, 0, length, 500, out int sz);
+			var err = reader.Read(buffer, 0, length, 200, out int sz);
 			if (err != LibUsbDotNet.Error.Success)
 			{
-				if (Logging)
-					Console.WriteLine($"Warning: winusb error {err} while reading payload. ({sz} read)");
+				Console.WriteLine($"Warning: winusb error {err} while reading payload. ({sz} read)");
 				return false;
 			}
 
@@ -236,7 +282,7 @@ namespace SysDVR.Client.Sources
 
 		public override void Flush()
 		{
-			reader.ReadFlush();
+			base.Flush();
 			ReadSize = 0;
 		}
 		
@@ -245,19 +291,12 @@ namespace SysDVR.Client.Sources
 		private int ReadSize = 0;
 		public override bool ReadHeader(byte[] buffer)
 		{
-			var err = writer.Write(USBMagic, 500, out int _);
-			if (err != LibUsbDotNet.Error.Success)
-			{
-				if (Logging)
-					Console.WriteLine($"Warning: libusb error {err} while requesting data");
-				return false;
-			}
+			using var trace = BeginTrace();
 
-			err = reader.Read(ReadBuffer, 0, PacketHeader.MaxTransferSize, 600, out ReadSize);
+			var err = reader.Read(ReadBuffer, 0, PacketHeader.MaxTransferSize, 400, out ReadSize);
 			if (err != LibUsbDotNet.Error.Success)
 			{
-				if (Logging)
-					Console.WriteLine($"Warning: libusb error {err} while reading header");
+				Console.WriteLine($"Warning: libusb error {err} while reading header");
 				return false;
 			}
 
