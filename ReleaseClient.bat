1801c512f06719fc52697eccb07afa8d8ff15d58
exelix11 <13405476+exelix11@users.noreply.github.com>
2022-07-03 17:04:28 +0200

    Restore ClientGUI packages before build

--- ReleaseClient.bat
+++ ReleaseClient.bat
@@ -33,6 +33,7 @@ dotnet publish -c Release || goto error
 
 cd ..\ClientGUI
 call VsDevCmd.bat
+msbuild ClientGUI.csproj -t:restore -p:RestorePackagesConfig=true
 msbuild ClientGUI.csproj /p:Configuration=Release || goto error
 
 cd ..
