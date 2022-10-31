3a8d93ee023e4749a295d4569098ab066e405fc1
DacoTaco <daco_65@hotmail.com>
2022-10-31 20:07:31 +0100

    fixes: updated gui client to use sdk style csproj & use more nuget packages

--- ClientGUI/ModesUI/PlayStreamControl.cs
+++ ClientGUI/ModesUI/PlayStreamControl.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SysDVRClientGUI
+namespace SysDVRClientGUI.ModesUI
 {
 	public partial class PlayStreamControl : UserControl, IStreamTargetControl
 	{
--- ClientGUI/ModesUI/RTSPStreamOptControl.Designer.cs
+++ ClientGUI/ModesUI/RTSPStreamOptControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SysDVRClientGUI
+﻿namespace SysDVRClientGUI.ModesUI
 {
 	partial class RTSPStreamOptControl
 	{
