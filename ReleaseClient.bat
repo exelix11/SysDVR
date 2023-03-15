b53268bb64a1a483384522eb48e78f3630de72f1
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-03-16 00:06:20 +0100

    CI Build script: third time's the charm

--- ReleaseClient.bat
+++ ReleaseClient.bat
@@ -2,6 +2,7 @@
 
 set PATH=C:\Program Files\7zip;C:\Program Files\7-Zip\;C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\;%PATH%
 
+:: Remember to also change the name of the folder in the zip file
 curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-02-28-12-37/ffmpeg-n5.1.2-39-g2953c6381a-win64-lgpl-shared-5.1.zip --output ffmpeg.zip
 
 if exist ffmpeg.zip (
@@ -10,7 +11,8 @@ if exist ffmpeg.zip (
 	
 	mkdir Client\runtimes\win-x64\native
 	
-	copy ffmpeg\ffmpeg-master-latest-win64-lgpl-shared\bin\*.dll Client\runtimes\win-x64\native
+	:: Chage this when the download URL changes
+	copy ffmpeg\ffmpeg-n5.1.2-39-g2953c6381a-win64-lgpl-shared-5.1\bin\*.dll Client\runtimes\win-x64\native
 )
 
 cd Client
