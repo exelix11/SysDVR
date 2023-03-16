af8f540bc9c6d13a14cd11cc6465277aea5c68e7
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-03-16 14:05:59 +0100

    Bump version to 5.5

--- sysmodule/source/modes/defines.h
+++ sysmodule/source/modes/defines.h
@@ -50,7 +50,7 @@ Debugging socket crash:
 #define ERR_SOCK_CONFIG_2 6
 
 //This is a version for the SysDVR Config app protocol, it's not shown anywhere and not related to the major version
-#define SYSDVR_VERSION 10
+#define SYSDVR_VERSION 11
 #define TYPE_MODE_USB 1
 #define TYPE_MODE_TCP 2
 #define TYPE_MODE_RTSP 4
