6e4f53c9945bf008a4b593d42895a1c2771e685f
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-03-19 17:09:17 +0100

    Single usb implementation, deprecate --no-winusb

--- Client/Sources/UsbStreaming.cs
+++ Client/Sources/UsbStreaming.cs
@@ -22,7 +22,6 @@ namespace SysDVR.Client.Sources
 		}
 
 		readonly LibUsbDotNet.LibUsb.UsbContext LibUsbCtx = null;
-		readonly bool ForceLibUsb;
 
 		private LogLevel _debugLevel;
 		public LogLevel DebugLevel {
@@ -41,10 +40,8 @@ namespace SysDVR.Client.Sources
 
 		public IUsbDevice device { get; private set; }
 		
-		public UsbContext(LogLevel logLevel = LogLevel.Error, bool forceLibUsb = false) 
+		public UsbContext(LogLevel logLevel = LogLevel.Error) 
 		{
-			ForceLibUsb = forceLibUsb;
-
 			LibUsbCtx = new LibUsbDotNet.LibUsb.UsbContext();
 
 			DebugLevel = logLevel;
@@ -120,15 +117,9 @@ namespace SysDVR.Client.Sources
             device = null;
         }
 
-		public UsbStreamingSourceBase CreateStreamingSource(bool HasVideo, bool HasAudio)
+		public UsbStreamingSource CreateStreamingSource(bool HasVideo, bool HasAudio)
 		{
-			UsbStreamingSourceBase source;
-            
-			// LibUsb backend on linux and windows doesn't seem to buffer reads from the pipe, this causes issues and requires manually receiving everything and copying data
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !ForceLibUsb)
-                source = new UsbStreamingSourceWinUsb(this);
-			else
-                source = new UsbStreamingSourceLibUsb(this);
+			var source = new UsbStreamingSource(this);
 
 			source.HasVideo = HasVideo;
 			source.HasAudio = HasAudio;
@@ -163,7 +154,7 @@ namespace SysDVR.Client.Sources
 		}
 	}
 
-	abstract class UsbStreamingSourceBase : IStreamingSource
+	class UsbStreamingSource : IStreamingSource
 	{
 		protected static readonly byte[] MagicRequestVideo = { 0xBB, 0xBB, 0xBB, 0xBB };
 		protected static readonly byte[] MagicRequestAudio = { 0xCC, 0xCC, 0xCC, 0xCC };
@@ -201,7 +192,7 @@ namespace SysDVR.Client.Sources
             _ => throw new Exception("Invalid state")
         };
 
-        public UsbStreamingSourceBase(UsbContext context)
+        public UsbStreamingSource(UsbContext context)
 		{
 			this.context = context;
 			(reader, writer) = context.OpenEndpointPair();
@@ -219,7 +210,7 @@ namespace SysDVR.Client.Sources
 			{
 				//using var trace = BeginTrace();
 #if DEBUG
-				Console.WriteLine($"Sending USB connection request");
+				Console.WriteLine($"Sending USB connection request {BitConverter.ToString(RequestMagic)}");
 #endif
                 var err = writer.Write(RequestMagic, 1000, out int _);
 				if (err != LibUsbDotNet.Error.Success)
@@ -247,87 +238,42 @@ namespace SysDVR.Client.Sources
             WaitForConnection();
 		}
 
-		// Provided by the underlying implementation
-		public abstract bool ReadHeader(byte[] buffer);
-		public abstract bool ReadPayload(byte[] buffer, int length);
+        // Not all USB implementations buffer data in the OS side,
+		// to support libusb we manually define the backing buffer and read everything in one shot
+		// At some point there was a different implementation for WinUSB since that does support buffering
+		// But it makes little sense to keep them separate as it doesn't grant any performance improvement.
+        private int ReadSize = 0;
+        private byte[] ReadBuffer = new byte[PacketHeader.MaxTransferSize];
+        public bool ReadHeader(byte[] buffer)
+        {
+            //using var trace = BeginTrace();
 
-		public void UseCancellationToken(CancellationToken tok)
+            var err = reader.Read(ReadBuffer, 0, PacketHeader.MaxTransferSize, 800, out ReadSize);
+            if (err != LibUsbDotNet.Error.Success)
+            {
+                if (Logging)
+                    Console.WriteLine($"Warning: libusb error {err} while reading header");
+                return false;
+            }
+
+            Buffer.BlockCopy(ReadBuffer, 0, buffer, 0, PacketHeader.StructLength);
+
+            return true;
+        }
+
+        public bool ReadPayload(byte[] buffer, int length)
+        {
+            if (length > ReadSize - PacketHeader.StructLength)
+                return false;
+
+            Buffer.BlockCopy(ReadBuffer, PacketHeader.StructLength, buffer, 0, length);
+
+            return true;
+        }
+
+        public void UseCancellationToken(CancellationToken tok)
 		{
 			Token = tok;
-		}
-	}
-
-	class UsbStreamingSourceWinUsb : UsbStreamingSourceBase
-	{
-		public UsbStreamingSourceWinUsb(UsbContext context) : base(context) { }
-
-		public override bool ReadHeader(byte[] buffer)
-		{
-			//using var trace = BeginTrace();
-
-			var err = reader.Read(buffer, 0, PacketHeader.StructLength, 200, out int _);
-			if (err != LibUsbDotNet.Error.Success)
-			{
-				if (Logging)
-					Console.WriteLine($"Warning: winusb error {err} while reading header");
-				return false;
-			}
-
-			return true;
-		}
-
-		public override bool ReadPayload(byte[] buffer, int length)
-		{
-			var err = reader.Read(buffer, 0, length, 200, out int sz);
-			if (err != LibUsbDotNet.Error.Success)
-			{
-				if (Logging)
-					Console.WriteLine($"Warning: winusb error {err} while reading payload. ({sz} read)");
-				return false;
-			}
-
-			return sz == length;
-		}
-	}
-
-	class UsbStreamingSourceLibUsb : UsbStreamingSourceBase
-	{
-		public UsbStreamingSourceLibUsb(UsbContext context) : base(context) { }
-
-		public override void Flush()
-		{
-			base.Flush();
-			ReadSize = 0;
-		}
-		
-		//Incredibly dumb libusb workaround
-		private byte[] ReadBuffer = new byte[PacketHeader.MaxTransferSize];
-		private int ReadSize = 0;
-		public override bool ReadHeader(byte[] buffer)
-		{
-			//using var trace = BeginTrace();
-
-			var err = reader.Read(ReadBuffer, 0, PacketHeader.MaxTransferSize, 400, out ReadSize);
-			if (err != LibUsbDotNet.Error.Success)
-			{
-				if (Logging)
-					Console.WriteLine($"Warning: libusb error {err} while reading header");
-				return false;
-			}
-
-			Buffer.BlockCopy(ReadBuffer, 0, buffer, 0, PacketHeader.StructLength);
-
-			return true;
-		}
-
-		public override bool ReadPayload(byte[] buffer, int length)
-		{
-			if (length > ReadSize - PacketHeader.StructLength)
-				return false;
-
-			Buffer.BlockCopy(ReadBuffer, PacketHeader.StructLength, buffer, 0, length);
-
-			return true;
 		}
 	}
 }
