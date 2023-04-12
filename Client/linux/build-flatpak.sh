262a5c7b9f9a0c11758bf57eaeda7008f4144538
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-04-12 21:12:50 +0200

    Revert to the manual flatpak build script

--- Client/linux/build-flatpak.sh
+++ Client/linux/build-flatpak.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-# Note that this is only used as a reference to build the flatpak locallu, CI uses an action directly
-
 set -e
+
+# install flatpak dependencies
+flatpak install --user flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08 runtime/org.freedesktop.Sdk.Extension.dotnet6/x86_64/22.08
 
 if [ ! -d "shared-modules" ]; then
 	git clone --depth 1 https://github.com/flathub/shared-modules.git
 fi
 
-dotnet publish ../Client.csproj -c Release 
+cd ..
+dotnet publish -c Release 
+cd linux
 
 if [ -d "dvr-build" ]; then
 	rm -rf dvr-build
 fi
 
 mkdir -p dvr-build
-mv ../bin/Release/net6.0/publish/* dvr-build/
-
-# install flatpak dependencies
-flatpak install --user --noninteractive --assumeyes flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08 runtime/org.freedesktop.Sdk.Extension.dotnet6/x86_64/22.08
+cp -r ../bin/Release/net6.0/publish/* dvr-build/
 
 flatpak-builder --user --install tmp com.github.exelix11.sysdvr.json --force-clean
 flatpak build-bundle ~/.local/share/flatpak/repo SysDVR-Client.flatpak com.github.exelix11.sysdvr --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
