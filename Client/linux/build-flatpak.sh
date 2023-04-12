4190b8762832240e65ff4c01cca37ee8995868f8
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-04-12 18:40:41 +0200

    Try to fix flatpak CI

--- Client/linux/build-flatpak.sh
+++ Client/linux/build-flatpak.sh
@@ -15,8 +15,16 @@ fi
 mkdir -p dvr-build
 mv ../bin/Release/net6.0/publish/* dvr-build/
 
-# install flatpak dependencies
-flatpak install --user --noninteractive --assumeyes flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08 runtime/org.freedesktop.Sdk.Extension.dotnet6/x86_64/22.08
+ARG=""
+# workaround for GitHub Actions always running as root
+if [ "$(id -u)" -eq 0 ]; then
+	ARG=""
+else
+	ARG="--user"
+fi
 
-flatpak-builder --user --install tmp com.github.exelix11.sysdvr.json --force-clean
+# install flatpak dependencies
+flatpak install $ARG --noninteractive --assumeyes flathub org.freedesktop.Platform//22.08 org.freedesktop.Sdk//22.08 runtime/org.freedesktop.Sdk.Extension.dotnet6/x86_64/22.08
+
+flatpak-builder $ARG --install tmp com.github.exelix11.sysdvr.json --force-clean
 flatpak build-bundle ~/.local/share/flatpak/repo SysDVR-Client.flatpak com.github.exelix11.sysdvr --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
