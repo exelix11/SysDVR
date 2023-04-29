4c5b470a4dfc679e51df0ef8d69bb107e7b103f0
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-04-29 13:09:22 +0200

    Better bat shortcuts for GUI

--- ClientGUI/IStreamTargetControl.cs
+++ ClientGUI/IStreamTargetControl.cs
@@ -15,9 +15,22 @@ namespace SysDVRClientGUI
 		Both = Video | Audio
 	}
 
-	interface IStreamTargetControl : IContainerControl
+    public class LaunchCommand
+    {
+        public string Executable { get; set; }
+        public string Arguments { get; set; }
+
+        public string[] FileDependencies { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"{Executable}\" {Arguments}";
+        }
+    }
+
+    interface IStreamTargetControl : IContainerControl
 	{
-		string GetCommandLine();
-		string GetExtraCmd();
+		string GetClientCommandLine();
+        LaunchCommand GetExtraCmd();
 	}
 }
