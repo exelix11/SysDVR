81f960f5b0aaeb3ba170e007220e64fc6034729d
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-08-27 01:30:10 +0200

    Check android dependencies with build script

--- Client/Platform/Android/buildbinaries.sh
+++ Client/Platform/Android/buildbinaries.sh
@@ -1,5 +1,49 @@
 #!/bin/sh
+
+set -e
+
+echo Checking dependencies...
+
+# Ensure SDL sources are present
+if [ ! -d "app/jni/SDL/src" ]; then
+	echo Downloading SDL sources...
+	wget https://github.com/libsdl-org/SDL/releases/download/release-2.28.1/SDL2-2.28.1.tar.gz
+	tar xf SDL2-2.28.1.tar.gz
+	mv $(pwd)/SDL2-2.28.1/src ./app/jni/SDL/
+	mv $(pwd)/SDL2-2.28.1/include ./app/jni/SDL/
+	rm -rf SDL2-2.28.1
+	rm SDL2-2.28.1.tar.gz
+fi
+
+# Ensure SDL_image sources are present
+if [ ! -d "app/jni/SDL_Image" ]; then
+	echo Downloading SDL_image sources...
+	wget https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.3/SDL2_image-2.6.3.tar.gz
+	tar xf SDL2_image-2.6.3.tar.gz
+	mv $(pwd)/SDL2_image-2.6.3 ./app/jni/SDL_Image
+	rm -rf SDL2_image-2.0.5
+	rm SDL2_image-2.6.3.tar.gz
+fi
+
+# Ensure cimgui sources are present
+if [ ! -d "app/jni/cimgui/cimgui" ]; then
+	echo Downloading cimgui sources...
+	wget https://github.com/exelix11/CimguiSDL2Cross/archive/refs/heads/master.zip
+	unzip master.zip > /dev/null
+	mv $(pwd)/CimguiSDL2Cross-master/cimgui ./app/jni/cimgui/
+	mv $(pwd)/CimguiSDL2Cross-master/Android.mk ./app/jni/cimgui/
+	rm -rf $(pwd)/CimguiSDL2Cross-master/
+	rm master.zip
+fi
+
+# Move to client root
 cd ../../
-bflat build --os:linux --arch:arm64 --libc:bionic --no-reflection --no-globalization -d ANDROID_LIB --ldflags "-soname libsysdvr.so" -r bin/Debug/net7.0/FFmpeg.AutoGen.dll -r bin/Debug/net7.0/LibUsbDotNet.dll -r bin/Debug/net7.0/RTSP.dll -r bin/Debug/net7.0/Microsoft.Extensions.Logging.Abstractions.dll
-mv Client Platform/Android/app/jni/sysdvr/libsysdvr.so
+
+# Seems to be needed to avoid conflicts with the dotnet build system
+rm -rf obj/
+
+echo Building client...
+bflat build --os:linux --arch:arm64 --libc:bionic --no-reflection --no-globalization -d ANDROID_LIB --ldflags "-soname libClient.so" -r bin/Debug/net7.0/FFmpeg.AutoGen.dll -r bin/Debug/net7.0/LibUsbDotNet.dll -r bin/Debug/net7.0/RTSP.dll -r bin/Debug/net7.0/Microsoft.Extensions.Logging.Abstractions.dll
+
+mv libClient.so Platform/Android/app/jni/Client/libClient.so
 cd Platform/Android/
