c487a2584a79f0e7f252b77a6254df68770dc5c1
exelix11 <13405476+exelix11@users.noreply.github.com>
2023-09-12 19:46:19 +0200

    Flatpak linux build

--- Client/Platform/Linux/build-flatpak.sh
+++ Client/Platform/Linux/build-flatpak.sh
@@ -9,16 +9,29 @@ if [ ! -d "shared-modules" ]; then
 	git clone --depth 1 https://github.com/flathub/shared-modules.git
 fi
 
-cd ..
+cd .. # Go to Platforms folder
+
+echo Checking dependencies...
+
+mkdir -p Resources/linux-x64/native
+
+if [ ! -e "Resources/linux-x64/native/cimgui.so" ]; then
+	echo Downloading Cimgui...
+	curl -L https://github.com/exelix11/CimguiSDL2Cross/releases/download/r1/linux-x64.zip -o linux-x64.zip
+	unzip linux-x64.zip
+	mv cimgui.so Resources/linux-x64/native/cimgui.so
+fi
+
+cd .. # Go to client root
 dotnet publish -c Release -r linux-x64
-cd linux
+cd Platform/Linux
 
 if [ -d "dvr-build" ]; then
 	rm -rf dvr-build
 fi
 
 mkdir -p dvr-build
-cp -r ../bin/Release/net7.0/linux-x64/publish/* dvr-build/
+cp -r ../../bin/Release/net7.0/linux-x64/publish/* dvr-build/
 
 flatpak-builder --user --install tmp com.github.exelix11.sysdvr.json --force-clean
 flatpak build-bundle ~/.local/share/flatpak/repo SysDVR-Client.flatpak com.github.exelix11.sysdvr --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
--- Client/Platform/Linux/flatpak-content/launcher.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dotnet /app/SysDVR-Client.dll $@
