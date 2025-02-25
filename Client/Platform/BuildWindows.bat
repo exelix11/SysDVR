457b59586bdc0773a7581ac7f69f9c5973b70b2d
exelix11 <13405476+exelix11@users.noreply.github.com>
2025-02-26 00:33:38 +0100

    Use my ffmpeg build mirror to build windows releases.
    Eventually we should migrate to 6.1 or any other LTS

--- Client/Platform/BuildWindows.bat
+++ Client/Platform/BuildWindows.bat
@@ -11,7 +11,7 @@ if exist Resources\win-x64\native\avcodec-59.dll (
 )
 
 echo Downloading ffmpeg binaries...
-curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2023-02-28-12-37/ffmpeg-n5.1.2-39-g2953c6381a-win64-lgpl-shared-5.1.zip --output ffmpeg.zip
+curl -L https://github.com/exelix11/sysdvr-ffmpeg-builds/releases/download/5.1.2-39-g2953c6381a/ffmpeg-n5.1.2-39-g2953c6381a-win64-lgpl-shared-5.1.zip --output ffmpeg.zip
 7z x ffmpeg.zip -olib_ffmpeg || goto error
 :: Chage this when the download URL changes
 copy lib_ffmpeg\ffmpeg-n5.1.2-39-g2953c6381a-win64-lgpl-shared-5.1\bin\*.dll Resources\win-x64\native\
