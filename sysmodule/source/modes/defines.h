57f89e2adf4b9ce9a880e1f99482d42c3faa7a2a
exelix <13405476+exelix11@users.noreply.github.com>
2025-11-17 22:52:57 +0100

    Bump version

--- sysmodule/source/modes/defines.h
+++ sysmodule/source/modes/defines.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // This is the main version shown to the user
-#define SYSDVR_VERSION_STRING  "6.2.1"
+#define SYSDVR_VERSION_STRING  "6.2.2"
 //This is a version for the SysDVR net and usb protocol, it's included in the beacon and may be shown in the UI. This is ascii so it's easy to insert in beacons for example as the serial of the USB device
 #define SYSDVR_PROTOCOL_VERSION "03"
 //This is a version for the SysDVR Config app protocol, it's not shown anywhere and not related to the major version
-#define SYSDVR_IPC_VERSION 16
+#define SYSDVR_IPC_VERSION 17
 
 #define SYSDVR_CRASH_MODULEID 0x69
 
